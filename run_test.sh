a01eb2912984af29f60a583776cb36e7bba127f6
Maderyx <Nevaghulius@gmail.com>
2026-05-09 17:07:35 +0200

    Add files via upload

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make && gcc Basic-libft-tester/test_libft.c Basic-libft-tester/stubs.c -L. -lft -o Basic-libft-tester/test_libft && ./Basic-libft-tester/test_libft
+make && gcc Basic-libft-tester/test_libft.c Basic-libft-tester/stubs.c -I. -L. -lft -o Basic-libft-tester/test_libft && ./Basic-libft-tester/test_libft
 make fclean
