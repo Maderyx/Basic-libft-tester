dada0b5662d355f67b18ea6c450aee7af23cbc45
Maderyx <Nevaghulius@gmail.com>
2026-05-10 20:08:11 +0200

    Add include path for gcc in run_test_lst.sh

--- run_test_lst.sh
+++ run_test_lst.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make && gcc Basic-libft-tester/test_libft_lst.c -L. -lft -o Basic-libft-tester/test_libft_lst && ./Basic-libft-tester/test_libft_lst
+make && gcc Basic-libft-tester/test_libft_lst.c -I. -L. -lft -o Basic-libft-tester/test_libft_lst && ./Basic-libft-tester/test_libft_lst
 make fclean
