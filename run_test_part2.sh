ed0fc718e9a7fc62f39a58dc3761d8a9565e06c3
Maderyx <Nevaghulius@gmail.com>
2026-05-10 20:08:32 +0200

    Add include path for gcc compilation in script

--- run_test_part2.sh
+++ run_test_part2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make && gcc Basic-libft-tester/test_libft_part2.c -L. -lft -o Basic-libft-tester/test_libft_part2 && ./Basic-libft-tester/test_libft_part2
+make && gcc Basic-libft-tester/test_libft_part2.c -I. -L. -lft -o Basic-libft-tester/test_libft_part2 && ./Basic-libft-tester/test_libft_part2
 make fclean
