c57ad015e32f109b640c488eccdb4a948945ab68
Maderyx <Nevaghulius@gmail.com>
2026-05-15 06:50:19 +0200

    added stubs.c to run_test_part2.sh

--- run_test_part2.sh
+++ run_test_part2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make && gcc Basic-libft-tester/test_libft_part2.c -I. -L. -lft -o Basic-libft-tester/test_libft_part2 && ./Basic-libft-tester/test_libft_part2
+make && gcc Basic-libft-tester/test_libft_part2.c Basic-libft-tester/stubs.c -I. -L. -lft -o Basic-libft-tester/test_libft_part2 && ./Basic-libft-tester/test_libft_part2
 make fclean
